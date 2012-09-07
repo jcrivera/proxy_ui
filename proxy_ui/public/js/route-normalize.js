6b3fec80a22dabe577379b237d9487cd7d4bdb73
Jrod <jarrod@jtms.net>
2012-09-06 23:57:08 -0400

    wip tenant route merging

--- proxy_ui/public/js/route-normalize.js
+++ proxy_ui/public/js/route-normalize.js
@@ -1,8 +1,18 @@
+window.parseUrl = function(url) {
+  var parsed_url = url.split(window.proxy_ui_host)[1].split('/');
+  parsed_url.shift();
+  return parsed_url
+}
+
 $(function() {
-  $('#proxy-ui a').each(function(index, elem) {
-    var href = $(elem).prop('href');
-    var parsed_url = href.split(window.proxy_ui_host)[1].split('/');
-    parsed_url.shift();
-    $(elem).prop('href', '/' + window.namespace + '/' + parsed_url.join('/'));
+  console.log(window.tenants);
+  console.log(window.location);
+
+  $(window.tenants).each(function(index, tenant) {
+    $('#' + tenant + ' a').each(function(index, elem) {
+      var href = $(elem).prop('href');
+      $(elem).prop('href', '/' + tenant + '/' + parseUrl(href).join('/'));
+    });
   });
+
 });
